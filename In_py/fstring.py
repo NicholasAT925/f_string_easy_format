150962cea34106b606364f8f2cb80b38c93bf187
Zuriel_Nicholas <2001555@sit.singaporetech.edu.sg>
2022-05-29 21:50:39 +0800

    Update fstring.py

--- In_py/fstring.py
+++ In_py/fstring.py
@@ -2,4 +2,5 @@ age = 24
 print("My " + f"age is {age}")
 # This is the cleanest way and easiest way to format the string to be printed
 # Example
-print(f"Pi is {22 / 7:12.50f}")
+print(f"Pi is {22 / 7:12.50f}") 
+#  Note: the "12.50f" stands for : Set 12 spaces for it and have the precision of 50 decimal place
